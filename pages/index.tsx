c2a0e190f8541c379db1f6b6c4cb387c52a290ff
Ismael Rumzan <ismael@cordobadigitalmedia.com>
2022-09-02 08:39:41 +0300

    ico file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import {
-  ArrowLeftCircleIcon,
-  ArrowRightCircleIcon,
-} from "@heroicons/react/24/outline";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Lawh</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Lawh Tablet</title>
+        <link rel="icon" href="/lawh.ico" />
       </Head>
       <div className="flex flex-col bg-[#E9D8BE] h-screen p-2">
         <div className="flex bg-[url('/lawh.png')] bg-contain bg-no-repeat bg-center h-full"></div>
